364fa444f01a919090894bc5caa975998385a2e6
BlackFox <BlackFox@MBP-Sitkin.Dlink>
2015-05-25 15:52:10 +1000

    Initial Commit

---
+++ kurs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  kurs
+//
+//  Created by BlackFox on 25.05.15.
+//  Copyright (c) 2015 BlackFox. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
